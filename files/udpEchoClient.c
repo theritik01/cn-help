fbd720b6a5ddd6cd4f41bf04d794301f78f4a1d2
Kevin-Aaaquil <eshansingh2409@gmail.com>
2022-11-24 01:00:45 +0530

    update code

--- files/udpEchoClient.c
+++ files/udpEchoClient.c
@@ -1,69 +1,59 @@
 // udpEchoClient.c
 
-//Server side implementation of UDP client-server model 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
 #define PORT 8080
-
 #define MAXLINE 1024
 
-Driver code int main() { int sockfd;
+// Driver code
+int main() {
+ int sockfd;
+ char buffer[MAXLINE];
+ char *hello = "Hello from server";
+ struct sockaddr_in servaddr, cliaddr;
 
-char buffer[MAXLINE];
+ // Creating socket file descriptor
+ if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+ perror("socket creation failed");
+ exit(EXIT_FAILURE);
+ }
 
-char *hello = "Hello from server"; struct sockaddr_in servaddr, cliaddr;
+ memset(&servaddr, 0, sizeof(servaddr));
+ memset(&cliaddr, 0, sizeof(cliaddr));
 
-Creating socket file descriptor
+ // Filling server information
+ servaddr.sin_family = AF_INET; // IPv4
+ servaddr.sin_addr.s_addr = INADDR_ANY;
+ servaddr.sin_port = htons(PORT);
 
-if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+ // Bind the socket with the server address
+ if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+ sizeof(servaddr)) < 0 )
+ {
+ perror("bind failed");
+ exit(EXIT_FAILURE);
+ }
+ int len, n;
 
-perror("socket creation failed");
+ len = sizeof(cliaddr); //len is value/result
 
-exit(EXIT_FAILURE);
+ n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+ MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+ &len);
+ buffer[n] = '\0';
+ printf("Client : %s\n", buffer);
+ sendto(sockfd, (const char *)hello, strlen(hello),
+ MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+ len);
+ printf("Hello message sent.\n");
 
-}
-
-memset(&servaddr, 0, sizeof(servaddr));
-
-memset(&cliaddr, 0, sizeof(cliaddr));
-
-Filling server information servaddr.sin_family = AF_INET; // IPv4 servaddr.sin_addr.s_addr = INADDR_ANY; servaddr.sin_port = htons(PORT);
-
-Bind the socket with the server address
-
-if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
-{
-
-perror("bind failed");
-
-exit(EXIT_FAILURE);
-
-}
-
-int len, n;
-
-len = sizeof(cliaddr); //len is value/result
-
-n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-
-MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-
-&len);
-
-buffer[n] = '\0';
-printf("Client : %s\n", buffer);
-
-sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-
-printf("Hello message sent.\n");
-
-return 0;
-
+ return 0;
 }
--- files/udpEchoServer.c
+++ files/udpEchoServer.c
@@ -1,57 +1,50 @@
 // udpEchoServer.c
-// Client side implementation of UDP client-server model
-#include <stdio.h>
 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
 #define PORT 8080
-
 #define MAXLINE 1024
 
-Driver code int main() { int sockfd;
+// Driver code
+int main() {
+ int sockfd;
+ char buffer[MAXLINE];
+ char *hello = "Hello from client";
+ struct sockaddr_in servaddr;
 
-char buffer[MAXLINE];
+ // Creating socket file descriptor
+ if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+ perror("socket creation failed");
+ exit(EXIT_FAILURE);
+ }
 
-char *hello = "Hello from client"; struct sockaddr_in servaddr;
+ memset(&servaddr, 0, sizeof(servaddr));
 
-Creating socket file descriptor
+ // Filling server information
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_port = htons(PORT);
+ servaddr.sin_addr.s_addr = INADDR_ANY;
 
-if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+ int n, len;
 
-perror("socket creation failed");
+ sendto(sockfd, (const char *)hello, strlen(hello),
+ MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+ sizeof(servaddr));
+ printf("Hello message sent.\n");
 
-exit(EXIT_FAILURE);
+ n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+ MSG_WAITALL, (struct sockaddr *) &servaddr,
+ &len);
+ buffer[n] = '\0';
+ printf("Server : %s\n", buffer);
 
+ close(sockfd);
+ return 0;
 }
-
-memset(&servaddr, 0, sizeof(servaddr));
-
-Filling server information servaddr.sin_family = AF_INET; servaddr.sin_port = htons(PORT); servaddr.sin_addr.s_addr = INADDR_ANY;
-
-int n, len;
-
-sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-printf("Hello message sent.\n");
-
-n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-
-MSG_WAITALL, (struct sockaddr *) &servaddr,
-
-&len);
-
-buffer[n] = '\0';
-
-printf("Server : %s\n", buffer);
-
-close(sockfd);
-
-return 0;
-
-}
-
