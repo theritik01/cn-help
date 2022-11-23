2f13d75b925d07a818106ced7873d5d55f3cb20d
Kevin-Aaaquil <eshansingh2409@gmail.com>
2022-11-23 23:27:37 +0530

    update code

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,87 @@ import fs from "fs";
   });
 
   app.get("/help", (req, res) => {
+    const data = fs.readFileSync("./files/help.txt", "utf8");
+    res.send(data);
+  });
+
+  app.get("/arpc", (req, res) => {
+    const data = fs.readFileSync("./files/arpClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/arps", (req, res) => {
+    const data = fs.readFileSync("./files/arpServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/cotcpc", (req, res) => {
+    const data = fs.readFileSync("./files/concurrentTcpClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/cotcps", (req, res) => {
+    const data = fs.readFileSync("./files/concurrentTcpServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/ftc", (req, res) => {
+    const data = fs.readFileSync("./files/fileTransferClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/fts", (req, res) => {
+    const data = fs.readFileSync("./files/fileTransferServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/fullc", (req, res) => {
+    const data = fs.readFileSync("./files/fullDuplexClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/fulls", (req, res) => {
+    const data = fs.readFileSync("./files/fullDuplexServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/halfc", (req, res) => {
+    const data = fs.readFileSync("./files/halfDuplexClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/halfs", (req, res) => {
+    const data = fs.readFileSync("./files/halfDuplexServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/rcc", (req, res) => {
+    const data = fs.readFileSync("./files/remoteCommandClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/rcs", (req, res) => {
+    const data = fs.readFileSync("./files/remoteCommandServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/tcpc", (req, res) => {
     const data = fs.readFileSync("./files/tcpClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/tcps", (req, res) => {
+    const data = fs.readFileSync("./files/tcpServer.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/udpc", (req, res) => {
+    const data = fs.readFileSync("./files/udpEchoClient.c", "utf8");
+    res.send(data);
+  });
+
+  app.get("/udps", (req, res) => {
+    const data = fs.readFileSync("./files/udpEchoServer.c", "utf8");
     res.send(data);
   });
 
