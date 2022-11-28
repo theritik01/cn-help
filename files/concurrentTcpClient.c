75bfb82994dce1d4887612b124f8a06127939fcb
Ritik Raj <99108259+theritik01@users.noreply.github.com>
2022-11-29 01:26:19 +0530

    Update concurrentTcpClient.c

--- files/concurrentTcpClient.c
+++ files/concurrentTcpClient.c
@@ -1,34 +1,14 @@
-// conncurrentTcpClient.c
-
-#include"netinet/in.h"
-#include"sys/socket.h"
-#include"stdio.h"
-main()
-{
-struct sockaddr_in sa,cli;
-int n,sockfd;
-int len;char buff[100];
-sockfd=socket(AF_INET,SOCK_STREAM,0);
-if(sockfd<0)
-{ printf("\nError in Socket");
-exit(0);
-}
-else printf("\nSocket is Opened");
-bzero(&sa,sizeof(sa));
-sa.sin_family=AF_INET;
-sa.sin_port=htons(5600);
-if(connect(sockfd,(struct sockaddr*)&sa,sizeof(sa))<0)
-{
-printf("\nError in connection failed");
-exit(0);
-}
-else
-printf("\nconnected successfully");
-if(n=read(sockfd,buff,sizeof(buff))<0)
-{
-printf("\nError in Reading");
-exit(0);
-}
-else
-{printf("\nMessage Read %s",buff);
-}}
+import socket
+import time
+def client_program():
+ host = socket.gethostname()
+ port = 6215
+ client_socket = socket.socket()
+ client_socket.connect((host, port))
+ while (True) :
+     data = client_socket.recv(1024).decode()
+     print('Received from server: ' + data)
+     break
+ client_socket.close()
+if __name__ == '__main__':
+ client_program()
